3d3e32a14e4691491c90012f8d6d28358cd9c5b3
刘哲一 <liuzheyi@liuzheyidemacbook-pro.local>
2016-02-02 22:29:32 -0500

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by 刘哲一 on 16/2/2.
+//  Copyright © 2016年 刘哲一. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
